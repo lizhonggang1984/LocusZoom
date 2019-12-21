763720198da45e7bda72ed0bf8cf3607f1e9ab65
Zhonggang Li <lizhonggang80@gmail.com>
2019-12-20 18:41:33 -0600

    Add files via upload

---
+++ ukbb_female.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+import os
+import sys
+
+# Find locuszoom binary. 
+sys.argv[0] = os.path.abspath(sys.argv[0]);
+lzbin = os.path.abspath(os.path.join(os.path.dirname(sys.argv[0]),"../bin/locuszoom"));
+
+# Run a quick example from the Kathiresan et al. data. 
+cmd = "%(bin)s --metal ./ukbb/female.txt --refgene SESN1 --flank 200kb" % {'bin' : lzbin};
+print "Running: %s" % cmd;
+os.system(cmd); 
